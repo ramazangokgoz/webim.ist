67e8404e0ff66d772d257aedc6837d527d3a8fa0
Ramazan GÖKGÖZ <38888193+ramazangokgoz@users.noreply.github.com>
2018-05-01 18:58:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-//WEBIM.IST//
-</v> web.ist_jav.iz </v>
+/* WEBIM.IST PROJESI - ARKABILISIM.COM */
+// WEBIM.IST - TÜRKİYE
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>WEBİM.İST PROJESİ</title>
+</head>
+
+<body>
+</body>
+</html>
